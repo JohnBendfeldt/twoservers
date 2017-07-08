5c0ac8ef414a20637b65b60986d711b86db09a41
John Bendfeldt <jfbendfeldt@gmail.com>
2017-07-08 11:25:03 -0500

    Add files via upload

---
+++ twoservers.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(happyPort).listen(7000);
+http.createServer(madPort).listen(7500);
+
+function happyPort (req, res) {
+  res.write('You are the best!\n');
+  res.end("It Works!! Path Hit: " + req.url);
+}
+
+function madPort (req, res) {
+  res.write('You are the worst!\n');
+  res.end("Bah Humbum! Path Hit: " + req.url);
+}
